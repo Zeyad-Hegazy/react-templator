e6aaa48eb19fb91de19d7345fefe71f46039e8a6
Zeyad Hegazy <zhegazy409@gmail.com>
2023-11-04 06:34:13 +0200

    Update templator.js
    delete old src folder

--- bin/templator.js
+++ bin/templator.js
@@ -1,4 +1,4 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
 
 const createNewSrc = () => {
@@ -20,6 +20,36 @@ const createNewSrc = () => {
 		"constants",
 	];
 
+	const files = [
+		{
+			file: "index.js",
+			data: `import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);`,
+		},
+		{
+			file: "App.jsx",
+			data: `import React from "react";
+import "./App.css";
+
+const App = () => {
+	return <div> App </div>;
+};
+
+export default App;
+`,
+		},
+		{ file: "index.css", data: `` },
+		{ file: "App.css", data: `` },
+	];
+
 	for (const folder of folders) {
 		fs.mkdir(path.join("src", folder), (err) => {
 			if (err) {
@@ -27,4 +57,19 @@ const createNewSrc = () => {
 			}
 		});
 	}
+
+	for (const file of files) {
+		fs.writeFile(
+			path.join("src", file.file),
+			file.data ? file.data : "",
+			(err) => {
+				if (err) {
+					console.error(`Failed to create file`);
+				}
+			}
+		);
+	}
 };
+
+fs.rmdirSync("./src", { recursive: true });
+createNewSrc();
