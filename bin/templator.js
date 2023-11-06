04f439092abc2eb9b8c85e4342e0d0a3e0678842
Zeyad Hegazy <zhegazy409@gmail.com>
2023-11-06 14:30:59 +0200

    Update templator.js

--- bin/templator.js
+++ bin/templator.js
@@ -2,46 +2,48 @@ import fs from "fs";
 import path from "path";
 import { componentContent } from "../assets/files-content.js";
 
-const createNewSrc = () => {
-	fs.mkdir("src", (err) => {
-		if (err) {
-			console.error(`Failed to create folder src:`, err);
-		}
-	});
+const folders = [
+	"components",
+	"pages",
+	"hoc",
+	"hooks",
+	"api",
+	"assets",
+	"util",
+	"ui",
+	"constants",
+];
 
-	const folders = [
-		"components",
-		"pages",
-		"hoc",
-		"hooks",
-		"api",
-		"assets",
-		"util",
-		"ui",
-		"constants",
-	];
-
-	const files = [
-		{
-			file: "index.js",
-			data: `import React from "react";
+const files = [
+	{
+		file: "index.js",
+		data: `import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import searchFolder from './../assets/error-handling/search';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 );`,
-		},
-		{
-			file: "App.jsx",
-			data: componentContent("App"),
-		},
-		{ file: "index.css", data: `` },
-		{ file: "App.module.css", data: `` },
-	];
+	},
+	{
+		file: "App.jsx",
+		data: componentContent("App"),
+	},
+	{ file: "index.css", data: `` },
+	{ file: "App.module.css", data: `` },
+];
+
+const createNewSrc = () => {
+	fs.mkdir("src", (err) => {
+		if (err) {
+			console.error(`Failed to create folder src:`, err);
+			return;
+		}
+	});
 
 	for (const folder of folders) {
 		fs.mkdir(path.join("src", folder), (err) => {
